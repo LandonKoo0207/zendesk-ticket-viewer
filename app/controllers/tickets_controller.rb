51c076586bbbc6c1388e8a3fa6163a954a2c5193
Landon Koo <landonkoo0207@gmail.com>
2019-06-09 15:21:41 +1000

    Feature for showing an Inividual ticket has initially been completed - overall formatting of the contents will be finalised later on

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,11 @@
 class TicketsController < ApplicationController
   def index
-    @tickets = Ticket.all(page: params[:page])
+    @tickets = Ticket.all(page: params[:page]).tickets
     @page_count = Ticket.page_count
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id]).ticket
+    @requester = User.find(@ticket.requester_id)
   end
 end
