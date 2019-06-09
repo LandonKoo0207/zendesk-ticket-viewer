54a0f6813b77032d422b33e7fa7a2cc674f22ed5
Landon Koo <landonkoo0207@gmail.com>
2019-06-09 12:38:15 +1000

    Ticket model has been modified to take parameters: (page: page number, per_page: number of tickets on a page, sort_by: sorting by ticket id, sort_order: descending order-newest ticket first)

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,7 @@
 class Ticket < Flexirest::Base
-  get :all, '/tickets.json'
+  TICKETS_PER_PAGE = 25
+
+  get :all, '/tickets.json', defaults: { page: 1, per_page: TICKETS_PER_PAGE, \
+                                         sort_by: 'id', sort_order: 'desc' }
   get :find, '/tickets/:id.json'
 end
